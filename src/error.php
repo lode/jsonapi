a0f7e45bd01cf560855d508da7379e343f859817
Lode Claassen <lode@alsvanzelf.nl>
2015-06-12 16:33:20 +0200

    always include a status code in errors
    useful when an exception has no preset code

--- src/error.php
+++ src/error.php
@@ -65,10 +65,8 @@ public function get_array() {
 	$response_part = array();
 	
 	// the basics
-	if ($this->http_status) {
-		$status_message = errors::get_http_status_message($this->http_status);
-		$response_part['status'] = $status_message;
-	}
+	$status_message = errors::get_http_status_message($this->http_status);
+	$response_part['status'] = $status_message;
 	$response_part['code'] = $this->error_message;
 	
 	// user guidance
