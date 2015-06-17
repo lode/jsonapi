cf80956d617962d47e372c438f04fce037ea2307
Lode Claassen <lode@alsvanzelf.nl>
2015-06-17 19:55:55 +0200

    update examples with current master

--- examples/errors_exception_direct.php
+++ examples/errors_exception_direct.php
@@ -8,13 +8,17 @@ require '../vendor/autoload.php';
 /**
  * via an exception
  * 
+ * @note previous exceptions will be added as well
+ * @note exceptions only output file, line, trace if the display_errors directive is true
+ *       you can tune it with that, or by setting jsonapi\base::$debug to false
  * @note echo'ing the exception has the same effect as using ->send_response()
  */
 
 try {
+	$http_status = \alsvanzelf\jsonapi\base::STATUS_NOT_FOUND;
 	$friendly_message = 'We don\'t know this user.';
 	$about_link = 'www.example.com/search';
-	throw new \alsvanzelf\jsonapi\exception('unknown user', 404, $previous=null, $friendly_message, $about_link);
+	throw new \alsvanzelf\jsonapi\exception('unknown user', $http_status, $previous=null, $friendly_message, $about_link);
 }
 catch (Exception $e) {
 	echo $e;
