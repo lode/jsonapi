824a0fd23d81b3d0516be344f7a6f836c4cc14a3
Lode Claassen <lode@alsvanzelf.nl>
2019-01-13 19:20:43 +0100

    wip for collection and empty-data documents

--- examples/v2.php
+++ examples/v2.php
@@ -1,6 +1,8 @@
 <?php
 
 use alsvanzelf\jsonapi\ResourceDocument;
+use alsvanzelf\jsonapi\CollectionDocument;
+use alsvanzelf\jsonapi\DataDocument;
 
 ini_set('display_errors', 1);
 error_reporting(-1);
@@ -21,5 +23,18 @@ echo '<pre>';
 $resource = new ResourceDocument($type, $id);
 $resource->addData($key, $value);
 $resource->sendResponse();
+
+echo '</pre><pre>';
+
+$collection = new CollectionDocument($type);
+$collection->addData($type, ($id*2), $array);
+$collection->addResource($resource);
+$collection->sendResponse();
+
+echo '</pre><pre>';
+
+$jsonapi = new DataDocument();
+$jsonapi->setHttpStatusCode(201);
+$jsonapi->sendResponse();
 
 echo '</pre>';
---
+++ src/CollectionDocument.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace alsvanzelf\jsonapi;
+
+use alsvanzelf\jsonapi\DataDocument;
+use alsvanzelf\jsonapi\exceptions\DuplicateException;
+use alsvanzelf\jsonapi\interfaces\ResourceInterface;
+use alsvanzelf\jsonapi\objects\ResourceObject;
+use alsvanzelf\jsonapi\objects\ResourceIdentifierObject;
+
+class CollectionDocument extends DataDocument {
+	public $resources = [];
+	private $resourceIdentifiers = [];
+	
+	/**
+	 * human api
+	 */
+	
+	public function addData($type, $id, array $attributes=[]) {
+		if ($attributes === []) {
+			$this->addResource(new ResourceIdentifierObject($type, $id));
+		}
+		else {
+			$resourceObject = new ResourceObject($type, $id);
+			$resourceObject->setData($attributes);
+			$this->addResource($resourceObject);
+		}
+	}
+	
+	/**
+	 * spec api
+	 */
+	
+	public function addResource(ResourceInterface $resource) {
+		$this->checkUsedResourceIdentifier($resource);
+		
+		$this->resources[] = $resource;
+		
+		$this->markUsedResourceIdentifier($resource);
+	}
+	
+	/**
+	 * output
+	 */
+	
+	public function toArray() {
+		$array = parent::toArray();
+		
+		$array['data'] = [];
+		foreach ($this->resources as $resource) {
+			$array['data'][] = get_object_vars($resource->getResource());
+		}
+		
+		return $array;
+	}
+	
+	/**
+	 * internal api
+	 */
+	
+	private function checkUsedResourceIdentifier(ResourceInterface $resource) {
+		$resourceKey = $resource->getResource()->type.'|'.$resource->getResource()->id;
+		if (isset($this->resourceIdentifiers[$resourceKey]) === false) {
+			return;
+		}
+		
+		throw new DuplicateException('can not have multiple resources with the same identification');
+	}
+	
+	private function markUsedResourceIdentifier(ResourceInterface $resource) {
+		$resourceKey = $resource->getResource()->type.'|'.$resource->getResource()->id;
+		$this->resourceIdentifiers[$resourceKey] = true;
+	}
+}
