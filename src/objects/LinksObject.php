5cf1fb2e53c088047d6f02cc9efc0f95bc77600a
Lode Claassen <lode@alsvanzelf.nl>
2019-02-28 19:33:08 +0100

    allow explicit null values for links and relationships

--- src/objects/LinksObject.php
+++ src/objects/LinksObject.php
@@ -111,6 +111,9 @@ class LinksObject implements ObjectInterface {
 			if ($link instanceof LinkObject && $link->isEmpty() === false) {
 				$array[$key] = $link->toArray();
 			}
+			elseif ($link === null || $link instanceof LinkObject && $link->isEmpty()) {
+				$array[$key] = null;
+			}
 			else {
 				$array[$key] = $link;
 			}
