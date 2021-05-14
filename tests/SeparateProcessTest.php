0c79e2cc728e1d2295f81a6296dcf34f846820c4
Lode Claassen <lode@alsvanzelf.nl>
2021-05-14 12:06:50 +0200

    tryout with more explicit xdebug call to satisfy scrutinizer

--- tests/SeparateProcessTest.php
+++ tests/SeparateProcessTest.php
@@ -48,25 +48,25 @@ class SeparateProcessTest extends TestCase {
 		ob_start();
 		$document->sendResponse();
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL], \xdebug_get_headers());
 		
 		$options = ['contentType' => Document::CONTENT_TYPE_OFFICIAL];
 		ob_start();
 		$document->sendResponse($options);
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL], \xdebug_get_headers());
 		
 		$options = ['contentType' => Document::CONTENT_TYPE_DEBUG];
 		ob_start();
 		$document->sendResponse($options);
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_DEBUG], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_DEBUG], \xdebug_get_headers());
 		
 		$options = ['contentType' => Document::CONTENT_TYPE_JSONP];
 		ob_start();
 		$document->sendResponse($options);
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_JSONP], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_JSONP], \xdebug_get_headers());
 	}
 	
 	/**
@@ -82,7 +82,7 @@ class SeparateProcessTest extends TestCase {
 		ob_start();
 		$document->sendResponse();
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL.';profile="https://jsonapi.org", '.Document::CONTENT_TYPE_OFFICIAL], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL.';profile="https://jsonapi.org", '.Document::CONTENT_TYPE_OFFICIAL], \xdebug_get_headers());
 		
 		$profile = new TestProfile();
 		$profile->setAliasedLink('https://jsonapi.org/2');
@@ -91,7 +91,7 @@ class SeparateProcessTest extends TestCase {
 		ob_start();
 		$document->sendResponse();
 		ob_end_clean();
-		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL.';profile="https://jsonapi.org https://jsonapi.org/2", '.Document::CONTENT_TYPE_OFFICIAL], xdebug_get_headers());
+		$this->assertSame(['Content-Type: '.Document::CONTENT_TYPE_OFFICIAL.';profile="https://jsonapi.org https://jsonapi.org/2", '.Document::CONTENT_TYPE_OFFICIAL], \xdebug_get_headers());
 	}
 	
 	/**
