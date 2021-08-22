30e6fb359ac24d618217ec9e165a4daa23ac28ce
Lode Claassen <lode@alsvanzelf.nl>
2021-08-22 22:51:32 +0200

    test extentions link for atomic operations in self link

--- tests/extensions/AtomicOperationsDocumentTest.php
+++ tests/extensions/AtomicOperationsDocumentTest.php
@@ -21,6 +21,7 @@ class AtomicOperationsDocumentTest extends TestCase {
 		$resource2->add('name', 'Arthur');
 		$resource3->add('name', 'Zaphod');
 		$document->addResults($resource1, $resource2, $resource3);
+		$document->setSelfLink('https://example.org/operations');
 		
 		$array = $document->toArray();
 		
@@ -28,6 +29,13 @@ class AtomicOperationsDocumentTest extends TestCase {
 		$this->assertArrayHasKey('ext', $array['jsonapi']);
 		$this->assertCount(1, $array['jsonapi']['ext']);
 		$this->assertSame((new AtomicOperationsExtension())->getOfficialLink(), $array['jsonapi']['ext'][0]);
+		
+		$this->assertArrayHasKey('links', $array);
+		$this->assertArrayHasKey('self', $array['links']);
+		$this->assertArrayHasKey('href', $array['links']['self']);
+		$this->assertArrayHasKey('type', $array['links']['self']);
+		$this->assertSame('https://example.org/operations', $array['links']['self']['href']);
+		$this->assertSame('application/vnd.api+json; ext="'.(new AtomicOperationsExtension())->getOfficialLink().'"', $array['links']['self']['type']);
 		
 		$this->assertArrayHasKey('atomic:results', $array);
 		$this->assertCount(3, $array['atomic:results']);
