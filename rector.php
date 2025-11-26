782d1cc2e34b36db7fbb0df2d18220db93aa5d72
Lode Claassen <lode@alsvanzelf.nl>
2025-11-26 18:00:33 +0100

    skip v1 paths for rector as well

--- rector.php
+++ rector.php
@@ -12,12 +12,21 @@ return RectorConfig::configure()
 		__DIR__ . '/tests',
 		__DIR__ . '/examples',
 	])
+	->withSkip([
+		__DIR__ . '/src/base.php',
+		__DIR__ . '/src/collection.php',
+		__DIR__ . '/src/error.php',
+		__DIR__ . '/src/errors.php',
+		__DIR__ . '/src/exception.php',
+		__DIR__ . '/src/resource.php',
+		__DIR__ . '/src/response.php',
+	])
 
 	// tab-based indenting
 	->withIndent(indentChar: "\t", indentSize: 1)
 
 	// slowly increase php version
-	->withPhpSets(php55: true)
+	->withPhpSets(php56: true)
 
 	// slowly increase levels
 	->withTypeCoverageLevel(1)
