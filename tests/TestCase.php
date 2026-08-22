72e42bbfbb85e5ba4e0e7526d3075e8a338448ac
Lode Claassen <lode@alsvanzelf.nl>
2026-08-22 20:33:20 +0200

    CS

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -5,18 +5,18 @@ declare(strict_types=1);
 namespace alsvanzelf\jsonapiTests;
 
 use PHPUnit\Framework\TestCase as PHPUnitTestCase;
+use PHPUnit\Runner\Version;
 
 class TestCase extends PHPUnitTestCase {
 	/**
 	 * cleanup once phpunit has `expectExceptionMessageIs()` (php ^8.4 and phpunit ^13.3)
 	 */
-    protected function expectExceptionMessageIs_(string $message): void
-    {
-        if (method_exists($this, 'expectExceptionMessageIs') === false) {
-        	parent::expectExceptionMessageMatches('/^'.preg_quote($message, delimiter: '/').'$/');
-        }
-        else {
-        	parent::expectExceptionMessageIs($message);
-        }
-    }
+	protected function expectExceptionMessageIs_(string $message): void {
+		if (version_compare(Version::id(), '13.3', '<')) {
+			parent::expectExceptionMessageMatches('/^'.preg_quote($message, delimiter: '/').'$/');
+		}
+		else {
+			parent::expectExceptionMessageIs($message);
+		}
+	}
 }
